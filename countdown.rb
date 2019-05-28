bc0942f624c08983b106b7313fab6df996492715
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-28 17:30:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(integer)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  while integer != 0
+    puts "#{integer} SECOND(S)!"
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
